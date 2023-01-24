c49f53b809ada2b454e74ec454618f77d3e52a38
sizakk <sizak.js@gmail.com>
2023-01-24 13:18:04 +0900

    first setting

---
+++ lib/component/rate_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RateCard extends StatelessWidget {
+  const RateCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(children: const []),
+    );
+  }
+}
+
+class _Scores extends StatelessWidget {
+  const _Scores();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
