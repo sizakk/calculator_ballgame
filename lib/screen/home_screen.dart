87c5868cb847f9e98879983361c78a892a2db6f6
sizakk <sizak.js@gmail.com>
2023-01-29 19:00:10 +0900

    commets update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: BACK_COLOR,
         body: Center(
           child: Column(
